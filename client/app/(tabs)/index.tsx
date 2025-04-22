53521ba5b78a8e0d07f24264021e5065124ce43a
cmezzac <christophermezzacappa818@yahoo.ca>
2025-04-22 15:47:19 -0400

    init

---
+++ client/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
